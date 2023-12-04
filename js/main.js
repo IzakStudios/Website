07ebe1b3b68352e7007dbc3995a37abbf51b4fad
IzakDev <141975061+IzakStudios@users.noreply.github.com>
2023-12-04 11:03:50 +0000

    Rotation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 window.addEventListener("load", () => {
-   document.getElementById("test").addEventListener("click", () => {
+   document.getElementById("close-sidebar").addEventListener("click", () => {
     Array.from(document.getElementsByClassName("sidebar")).forEach((element) => {
         let isHidden = element.classList.contains("hidden");
         if (isHidden) return element.classList.remove("hidden");
+
         element.classList.add("hidden");
     })
    })
